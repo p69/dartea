3bb373ff87b98fb2b3830371f9b95a164e2b151f
pavel-shilyagov <shilyagov.p@gmail.com>
2018-05-14 13:13:00 +0300

    comments

--- lib/dartea.dart
+++ lib/dartea.dart
@@ -11,6 +11,20 @@ part 'src/cmd.dart';
 part 'src/widget.dart';
 
 /// Container object for applcation's functions
+/// [init] - creates inital state of a [TModel] type
+/// [update] - handle all messages of [TMsg] type and current model.
+/// Returns [Upd<TModel, TMsg>] with new model and (optionally) commands (see [Cmd] class)
+/// [view] - gets [BuildContext], [Dispatch<TMsg>] and [TModel] and returns Flutter's Widgets tree.
+/// [onError] - handler for all errors inside program loop
+/// [subscription] - separate handler for manage subscriptions to external events.
+/// Gets subscription holder of [TSub] (for example [StreamSubscription] or some custom class), [Dispatch<TMSg>] and current model.
+/// Subscription handler calls after every model's changes (the same as [update] function).
+/// If you need to save subscription holder then return it as a function result.
+/// If subscription is disposed then just return null (or create new).
+/// [lifeCycleUpd] - separate handler for global app lifecycle events.
+/// Gets [AppLifeCycleState] and current model of [TModel].
+/// Returns [Upd<TModel, TMsg>] with new model and (optionally) commands (see [Cmd] class).
+/// It could be useful for saving state, cancelling some operation or disposing subscription.
 class Program<TModel, TMsg, TSub> {
   final Init<TModel, TMsg> init;
   final Update<TModel, TMsg> update;
@@ -29,7 +43,7 @@ class Program<TModel, TMsg, TSub> {
         this.lifeCycleUpdate =
             lifeCycleUpd != null ? lifeCycleUpd : ((_, __) => null);
 
-  ///wrap all functions with [debugPrint]
+  ///Wrap all functions with [debugPrint]
   Program<TModel, TMsg, TSub> withDebugTrace() {
     return Program(
         () {
@@ -60,7 +74,7 @@ class Program<TModel, TMsg, TSub> {
         onError: onError);
   }
 
-  ///Create widget chich could be inserted into Flutter application
+  ///Create widget which could be inserted into Flutter application
   Widget build({Key key}) {
     return new DarteaWidget(this, key: key);
   }
